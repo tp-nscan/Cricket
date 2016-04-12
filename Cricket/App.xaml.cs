21a336d69131d62fa66cced586bb620ee18b40d3
jemboo <jemboo@gmail.com>
2016-04-11 21:43:48 -0400

    last changes

--- Cricket/App.xaml.cs
+++ Cricket/App.xaml.cs
@@ -10,14 +10,14 @@ namespace Cricket
     {
         public App()
         {
-            using (var ctx = new NodePadContext())
-            {
-                var tc = new Tc { FirstName = "Ralph", LastName = "Malph", MiddleName = "Winston" };
-                var ms = new MatrixInfo { ValBnd = Data.ValBnd.B, Descr = "mi descr", MatrixShape = Data.MatrixShape.D1, Shape = "Ms Shape", Values = "ms values"};
-                ctx.Tcs.Add(tc);
-                ctx.MatrixInfos.Add(ms);
-                ctx.SaveChanges();
-            }
+            //using (var ctx = new NodePadContext())
+            //{
+            //    var tc = new Tc { FirstName = "Ralph", LastName = "Malph", MiddleName = "Winston" };
+            //    var ms = new MatrixInfo { ValBnd = Data.ValBnd.B, Descr = "mi descr", MatrixShape = Data.MatrixShape.D1, Shape = "Ms Shape", Values = "ms values"};
+            //    ctx.Tcs.Add(tc);
+            //    ctx.MatrixInfos.Add(ms);
+            //    ctx.SaveChanges();
+            //}
 
         }
     }
